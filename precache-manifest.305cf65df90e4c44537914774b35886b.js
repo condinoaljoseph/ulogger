be9f7bead2fc7d69a6adb7e6be8f65612a4bec73
Al Joseph Condino <condinoaljoseph@gmail.com>
2020-02-19 14:38:30 +0800

    Updates

---
+++ precache-manifest.305cf65df90e4c44537914774b35886b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f4e643d05976f116e1dd7deee7e9dd",
+    "url": "/ulogger/index.html"
+  },
+  {
+    "revision": "e7901d1149e6654e1f0a",
+    "url": "/ulogger/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "1d9aaf1e66d29a84be1a",
+    "url": "/ulogger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e7901d1149e6654e1f0a",
+    "url": "/ulogger/static/js/2.1129d885.chunk.js"
+  },
+  {
+    "revision": "5e589bd0256ee07ece3bc7045fd89391",
+    "url": "/ulogger/static/js/2.1129d885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9aaf1e66d29a84be1a",
+    "url": "/ulogger/static/js/main.7c76ca24.chunk.js"
+  },
+  {
+    "revision": "2ebb1998b3df57add5ff",
+    "url": "/ulogger/static/js/runtime-main.1693ad65.js"
+  }
+]);
